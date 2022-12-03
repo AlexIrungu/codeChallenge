ce43638855a4e467950eca24868fb0a0c07b73ea
AlexIrungu <aleaxmuiruri@gmail.com>
2022-12-03 13:52:35 +0300

    Completed assignment

--- speeddetector.js
+++ speeddetector.js
@@ -1,18 +1,21 @@
 //code challenge 2
 //prompt command: every time the speed test runs
 const prompt = require('prompt-sync')();
-//the user is asked to input marks
+//the user is asked to input speed
 let speed = prompt('Please enter speed: ')
 //the function speedDetector is declared
 function speedDetector(speed){
+    //the speed poutputs ok if the speed is less than 70
     if(speed < 70){
         return 'Ok'
+        //if the speed is equal or greater than 70, it calculates the different demerit points
     }else if(speed >= 70){
-        let velocity = (speed - 70) / 5
-        if (velocity > 12){
+        let demerit = (speed - 70) / 5
+        //if the demerit points are greater than 12, it outputs License suspended
+        if (demerit > 12){
             return ("License suspended")
         }else{
-            return ("Demerit: " + velocity)
+            return ("Demerit: " + demerit)
         }
     }
 }
